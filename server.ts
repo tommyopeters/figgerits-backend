001687c81fea5d0583d3bb26d0137829e426e39a
Tommy O'Peters <tommyopeters@gmail.com>
2024-08-25 20:10:59 +0100

    Refactor server.ts to enable CORS and use express middleware for JSON parsing

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
